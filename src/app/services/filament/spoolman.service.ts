36288a189dde8e816c5237931b91467bf5ec0f99
Hilton Shumway <hillshum@gmail.com>
2025-02-24 19:53:12 -0600

    Correctly update spoolman spool

--- src/app/services/filament/spoolman.service.ts
+++ src/app/services/filament/spoolman.service.ts
@@ -64,10 +64,8 @@ export class SpoolmanOctoprintService implements FilamentPluginService {
 
   public setSpool(spool: FilamentSpool): Observable<void> {
     const setSpoolBody = {
-      selection: {
-        toolIdx: 0,
-        spoolId: spool.id,
-      },
+      toolIdx: 0,
+      spoolId: spool.id,
     };
 
     return this.http.post<void>(
